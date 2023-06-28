1d9eb4bc944daaf8d4066a537c82adf7661f5c56
matheuscezar <matheuscezar@hotmail.com>
2023-06-27 22:50:45 -0300

    Update waf_bypass.sh

--- security/bash-script/waf_bypass.sh
+++ security/bash-script/waf_bypass.sh
@@ -1,1 +1,2 @@
+cat security-trails.txt | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort -u > ips.txt
 for ip in $(cat ips.txt); do  echo -n "Testing IP $ip - "; curl "https://$ip/" -s -o /dev/null -w '%{http_code}' -H 'Host: victims.com' -k --connect-timeout 5; echo "";  done
