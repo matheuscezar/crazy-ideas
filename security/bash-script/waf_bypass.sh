fa2c014bdbd99bbe64b0b34d0cf0d2c31416f0b9
matheuscezar <matheuscezar@hotmail.com>
2023-09-15 10:35:43 -0300

    Update waf_bypass.sh

--- security/bash-script/waf_bypass.sh
+++ security/bash-script/waf_bypass.sh
@@ -4,4 +4,4 @@ for ip in $(cat ips.txt); do  echo -n "Testing IP $ip - "; curl "https://$ip/" -
 
 
 #bypass por ASN
-for i in $(seq 1 255); do echo -n "186.233.250.$i: " ; curl "https://186.233.250.$i" -s -o /dev/null -w '%{http_code}' -k -H "Host: www.hostgator.com.br" --connect-timeout 5; echo "" ; done
+for i in $(seq 1 255); do echo -n "x.x.x.$i: " ; curl "https://186.233.250.$i" -s -o /dev/null -w '%{http_code}' -k -H "Host: victims.com" --connect-timeout 5; echo "" ; done
